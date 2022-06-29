08bc23976256449d5eacdd41c948c64b235ece1a
pratik <pratik.varute@joshsoftware.com>
2022-06-29 16:53:31 +0530

    adding structures for random user genration response

---
+++ External-Api/model/credit_card.go
@@ -0,0 +1,5 @@
+package model
+
+type Credit_card struct {
+	Cc_number string `json:cc_number`
+}
---
+++ External-Api/model/user.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+type User struct {
+	ID                      int64       `json:id`
+	UID                     string      `json:uid`
+	Password                string      `json:password`
+	First_name              string      `json:first_name`
+	Last_name               string      `json:last_name`
+	Username                string      `json:username`
+	Email                   string      `json:email`
+	Avatar                  string      `json:avatar`
+	Gender                  string      `json:gender`
+	Phone_number            string      `json:phone_number`
+	Social_insurance_number string      `json:social_insurance_number`
+	Date_of_birth           time.Time   `json:date_of_birth`
+	Employment              Employment  `json:"employment"`
+	Credit_card             Credit_card `json:Credit_card`
+}
