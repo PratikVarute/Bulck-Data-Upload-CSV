a0a337a607e3c7819d334c203a27f5ce447ebea3
pratik <pratik.varute@joshsoftware.com>
2022-07-04 11:20:53 +0530

    Updateted code

---
+++ External-Api/model/payment/creditCard.go
@@ -0,0 +1,5 @@
+package payment
+
+type CreditCard struct {
+	CcNumber string `json:"cc_number"`
+}
---
+++ External-Api/model/subscription/subcription.go
@@ -0,0 +1,8 @@
+package subscription
+
+type Subscription struct {
+	Plan          string `json:"plan"`
+	Status        string `json:"status"`
+	PaymentMethod string `json:"payment_method"`
+	Term          string `json:"term"`
+}
